26729c29349396f109edcfb33e8a28afa114a7ad
Kif_ <64006969+Keflemariam@users.noreply.github.com>
2022-04-05 22:05:45 +0300

    Post

---
+++ src/app/posts/posts.component.ts
@@ -0,0 +1,15 @@
+import { Http } from '@angular/http';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-posts',
+  templateUrl: './posts.component.html',
+  styleUrls: ['./posts.component.css']
+})
+export class PostsComponent  {
+  constructor(http:Http){
+    
+  }
+  
+
+}
